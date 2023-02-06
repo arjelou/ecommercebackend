4002dc65c62fc7ff6f2faee7ec10dc6be6500d8b
Jelou <arjelou.jelou@gmail.com>
2023-02-06 17:41:53 +0800

    create table for users

---
+++ migrations/20230206070912-create-user.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      company: {
+        type: Sequelize.STRING
+      },
+      total_employee: {
+        type: Sequelize.INTEGER
+      },
+      zipcode: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      confirmpassword: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ models/user.js
@@ -0,0 +1,39 @@
+'use strict';
+const {DataTypes, Sequelize} = require('sequelize');
+const instance = require('../dbconnection');
+
+const user = instance.sequelize.define('users', {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  email: {
+    type: DataTypes.STRING
+  },
+  company: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  total_employee: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  zipcode: {
+    type: DataTypes.STRING
+  },
+  password: {
+    type: DataTypes.STRING
+  },
+  confirmpassword: {
+    type: DataTypes.STRING
+  },
+  
+},
+{
+  createdAt: true,
+  updatedAt: true,
+  tableName: 'users',
+})
+exports.model = user;
