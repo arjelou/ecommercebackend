81508e5ee44560be62220256891239fc69b8ffec
Jelou <arjelou.jelou@gmail.com>
2023-02-15 17:23:30 +0800

    fix id

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/productControllers');
 
-router.get('/',controller.getProduct)
+router.get('/product',controller.getProduct)
 router.post('/addnew',controller.addProduct)
 router.delete('/product/:id',controller.deleteProduct)
 router.put('/update/:id',controller.updateProduct)
