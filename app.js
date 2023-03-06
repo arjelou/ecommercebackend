0553e26025b288c1fe18b9c42e6349d896c7a9f7
Jelou <arjelou.jelou@gmail.com>
2023-03-06 14:22:40 +0800

    new port

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 
 require('dotenv').config({path: "./.env"})
 
-app.use(cors({origin: ['https://ecommerce-arjelou.vercel.app/']}))
-// app.use(cors({origin: ['http://localhost:3000']}))
+// app.use(cors({origin: ['https://ecommerce-arjelou.vercel.app/']}))
+app.use(cors({origin: ['http://localhost:3000']}))
 // app.use(cors('*'))
 
 
@@ -17,7 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/', function(req, res) {
     res.send('WELCOME TO MY API, just user /product');
 })
-// app.use('/addnew', productRoutes)
+app.use('/addnew', productRoutes)
 
-// app.listen(process.env.MYSQLPORT || PORT);
-app.listen(process.env.MYSQLPORT);
+app.listen(process.env.MYSQLPORT || PORT);
