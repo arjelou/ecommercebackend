4c3cd926c1e16f72409999ab3fb996e93c9588d1
Jelou <arjelou.jelou@gmail.com>
2023-03-06 11:16:38 +0800

    change routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/productControllers');
 
-router.get('/product',controller.getProduct)
+router.get('',controller.getProduct)
 router.post('/addnew',controller.addProduct)
 router.delete('/product/:id',controller.deleteProduct)
 router.put('/update/:id',controller.updateProduct)
