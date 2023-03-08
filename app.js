9748525662d7e9e50528fad561950493d4fdcf57
Jelou <arjelou.jelou@gmail.com>
2023-03-08 16:22:55 +0800

    production url

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const cors = require('cors');
 
 require('dotenv').config({path: "./.env"})
 
-// app.use(cors({origin: ['https://ecommerce-arjelou.vercel.app/']}))
+app.use(cors({origin: ['https://ecommerce-blue-gamma.vercel.app/signup']}))
 // app.use(cors({origin: ['http://localhost:3000']}))
-app.use(cors('*'))
+// app.use(cors('*'))
 
 
 app.use(express.json());
