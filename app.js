c68ef4e100e0f3e0ce75e454381733fa3a30cdc1
Jelou <arjelou.jelou@gmail.com>
2023-03-08 17:00:49 +0800

    cors policy

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 require('dotenv').config({path: "./.env"})
 
-app.use(cors({origin: ['https://ecommerce-blue-gamma.vercel.app/']}))
+app.use(cors({origin: [`https://ecommerce-blue-gamma.vercel.app`]}))
 // app.use(cors('*'))
 
 
